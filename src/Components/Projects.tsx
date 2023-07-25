1b1ed2b0d99f78193577f74cd019b6e40a03b0cb
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-07-25 17:14:36 +0200

    new fetchProject demo

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,4 +1,19 @@
+import { db } from "../Components/FBconfig";
+import { query, collection } from "firebase/firestore";
+
+import { useEffect, useState } from "react";
+
 const projects = () => {
+  const [projectsArray, setProjectsArray] = useState([]);
+
+  function fetchProjects() {
+    const ProjectsRef = collection(db, "projects");
+  }
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
   return <div>projects</div>;
 };
 
