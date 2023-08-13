802dc05325aa12c5685956cb9c994d1c6cb717eb
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-08-13 11:27:48 +0200

    css for responsive video + active link functionality

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,41 @@
+import React, { useState } from "react";
+
 const Navbar = () => {
+  const [activeLink, setActiveLink] = useState<string | null>(null);
+  console.log("Active link:", activeLink);
+
   return (
     <div id="navbar">
-      <a href="#section1">Home</a>
-      <a href="#section2">About Me</a>
-      <a href="#section3">Projects</a>
-      <a href="#section4">Contact</a>
+      <a
+        href="#section1"
+        className={activeLink === "home" ? "active-link" : ""}
+        onClick={() => {
+          setActiveLink("home");
+        }}
+      >
+        Home
+      </a>
+      <a
+        href="#section2"
+        className={activeLink === "about" ? "active-link" : ""}
+        onClick={() => setActiveLink("about")}
+      >
+        About Me
+      </a>
+      <a
+        href="#section3"
+        className={activeLink === "services" ? "active-link" : ""}
+        onClick={() => setActiveLink("services")}
+      >
+        Projects
+      </a>
+      <a
+        href="#section4"
+        className={activeLink === "contact" ? "active-link" : ""}
+        onClick={() => setActiveLink("contact")}
+      >
+        Contact
+      </a>
     </div>
   );
 };
